fb2a4c3f7113de3289474079288beff8874d0e9a
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-08-08 17:09:28 -0300

    Adicionando base do projeto.

---
+++ src/routers.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/dashboard/Dashboard'
+import Login from './pages/login/Login'
+import People from './pages/people/People'
+
+const Routers = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={Dashboard}></Route>
+        <Route path="/login" element={Login}></Route>
+        <Route path="/pessoa" element={People}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+export default Routers
