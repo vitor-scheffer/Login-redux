8899565c3bcf3f144231fde3a5313c28c890fe6f
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-08-11 12:51:39 -0300

    Notfound.

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
     <PageNotFound>
       <TitleNotFound>Página não encontrada</TitleNotFound>
       <img src={astronaut} alt="" />
-      <Link to="/login"><TitleNotFound>Back to home</TitleNotFound></Link>
+      <Link to="/pessoa"><TitleNotFound>Back to home</TitleNotFound></Link>
     </PageNotFound>
   )
 }
